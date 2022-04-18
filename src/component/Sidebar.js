e91f087ad95ababed5ab9e2282785cd421d99444
wcakhtarhusen <akhtarhusen@webcluesinfotech.com>
2022-04-18 18:15:09 +0530

    menu changes

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -4,7 +4,7 @@ import { slide as Menu } from "react-burger-menu";
 export default props => {
     return (
         // Pass on our props
-        <Menu {...props}>
+        <Menu {...props} right >
             <a className="menu-item" href="/">
                 Home
             </a>
